5126ea065f596efd03e578aea31e02ef95940e10
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-09-04 07:17:54 +0800

    Mobile design navbar

---
+++ src/Components/Pages/HomePageMobile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MobileNavBar from '../Parts/MobileNavBar'
+import Banner from '../Parts/Banner'
+import Insights from '../Parts/Insights'
+import WhoWeAre from '../Parts/WhoWeAre'
+import Events from '../Parts/Events'
+import Footer from '../Parts/Footer'
+
+export default function HomePageMobile() {
+  return (
+    <>
+      <MobileNavBar />
+    </>
+  )
+}
